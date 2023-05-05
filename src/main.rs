030fdbe7c96d8af281c780d6e4297842215708df
Deniz Demir <darkenedMellow@protonmail.com>
2023-05-05 20:06:07 +0300

    Added automatic channel creation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::fs::File;
 use tokio::task::{self, JoinHandle};
 
+use rand::{distributions::Alphanumeric, Rng};
 use serde::{Deserialize, Serialize};
 use serenity::{
     async_trait,
-    model::prelude::{Channel, ChannelId, GuildId, Message, Ready},
+    model::prelude::{Channel, ChannelId, ChannelType, GuildId, Message, Ready},
     prelude::{Context, EventHandler, GatewayIntents},
     Client, Error,
 };
@@ -14,6 +15,7 @@ struct Config {
     pub token: String,
     pub guild_id: u64,
     pub ping_id: u64,
+    pub create_channel: bool,
 }
 
 impl Config {
@@ -21,6 +23,9 @@ impl Config {
         serde_json::from_reader(reader)
     }
 }
+
+const CHANNEL_NAME_LEN: usize = 50;
+const CHANNEL_AMOUNT: usize = 1000;
 
 struct Handler;
 
@@ -33,9 +38,67 @@ impl EventHandler for Handler {
 
         let guild = GuildId(config.guild_id);
 
+        if config.create_channel {
+            println!("Creating channels...");
+
+            if CHANNEL_AMOUNT > 500 {
+                println!("Cannot create more than 500 channels");
+                return;
+            }
+
+            if CHANNEL_AMOUNT > 10 {
+                let threads = (CHANNEL_AMOUNT - (CHANNEL_AMOUNT % 10)) / 10;
+                let remainder = CHANNEL_AMOUNT % 10;
+
+                let mut handles: Vec<JoinHandle<()>> = Vec::new();
+
+                for _ in 0..threads {
+                    let ctx = ctx.clone();
+
+                    let handle = task::spawn(async move {
+                        for _ in 0..10 {
+                            let channel_name: String = rand::thread_rng()
+                                .sample_iter(&Alphanumeric)
+                                .take(CHANNEL_NAME_LEN)
+                                .map(char::from)
+                                .collect();
+
+                            guild
+                                .create_channel(ctx.http.clone(), |c| {
+                                    c.name(channel_name).kind(ChannelType::Text)
+                                })
+                                .await
+                                .unwrap();
+                        }
+                    });
+
+                    handles.push(handle);
+                }
+
+                for _ in 0..remainder {
+                    let channel_name: String = rand::thread_rng()
+                        .sample_iter(&Alphanumeric)
+                        .take(CHANNEL_NAME_LEN)
+                        .map(char::from)
+                        .collect();
+
+                    guild
+                        .create_channel(ctx.clone().http.clone(), |c| {
+                            c.name(channel_name).kind(ChannelType::Text)
+                        })
+                        .await
+                        .unwrap();
+                }
+            }
+
+            println!("Done creating channels!");
+        }
+
         let channels = guild.channels(&ctx).await.unwrap();
 
         let mut handles: Vec<JoinHandle<Result<Message, Error>>> = Vec::new();
+
+        println!("Pinging...");
 
         for (channel_id, _) in &channels {
             let channel = ChannelId((*channel_id).into());
